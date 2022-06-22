1833acec5089b247b2db5dc80483daeb56a2cf05
FredoStanley <franaivoson@ingenosya.mg>
2022-06-22 14:31:49 +0300

    maj template

--- igy-templates-modules/lib/global-constants.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class GlobalConstants {
-    static imgURL: string;
-}
--- igy-templates-modules/lib/layouts/authentification/authentification.component.d.ts
+++
@@ -1,8 +0,0 @@
-import { OnInit } from '@angular/core';
-import * as i0 from "@angular/core";
-export declare class AuthentificationComponent implements OnInit {
-    constructor();
-    ngOnInit(): void;
-    static ɵfac: i0.ɵɵFactoryDeclaration<AuthentificationComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<AuthentificationComponent, "igy-authentification", never, {}, {}, never, ["[formLogin]"]>;
-}
